aeb78d077dcb4cd94fc2e78835177522a4440301
oAnshull <oanshul6969@gmail.com>
2025-05-12 23:03:16 +0530

    forgot these

--- src/Elements/Element.hpp
+++ src/Elements/Element.hpp
@@ -6,6 +6,6 @@
 class Element
 {
     std::string name; //Element Name/Indentifier
-    Element* parent;
-    std::vector<Element*> children;
+    Element* parent; //Parent Element of this Element
+    std::vector<Element*> children; //Child Elements of this Element
 };
