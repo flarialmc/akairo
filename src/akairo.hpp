d2105a3219cd9b6805be880d1610f167d964278d
Bari <58800830+TheBarii@users.noreply.github.com>
2025-05-25 16:10:29 +0600

    Update akairo.hpp

--- src/akairo.hpp
+++ src/akairo.hpp
@@ -8,7 +8,7 @@
 namespace akairo {
 
     static std::unordered_map<std::string, std::unique_ptr<Renderer::Interface>> renderers;
-    static bool CreateRenderer(const std::string& name, Renderer::BackendType backend, Renderer::FrameworkType framework, int Width, int Height);
+    bool CreateRenderer(const std::string& name, Renderer::BackendType backend, Renderer::FrameworkType framework, int Width, int Height);
 
     static std::unique_ptr<Renderer::Interface> GetRenderer(const std::string& name);
 };
