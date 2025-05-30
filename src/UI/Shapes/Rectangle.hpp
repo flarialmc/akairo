1ce528b77aaf45db8bd9ab150a783d7fbd8b41ca
Bari <58800830+TheBarii@users.noreply.github.com>
2025-05-31 02:12:18 +0600

    heat for later

--- src/UI/Shapes/Rectangle.hpp
+++ src/UI/Shapes/Rectangle.hpp
@@ -4,6 +4,14 @@
 namespace akairo::Shapes {
     class Rectangle final : public Shape {
     public:
+
+        /*
+         * In the future, have a different constructor that takes in just the constraint values for each component.
+         * And a hex perhaps, for the color.
+         * It would look something like this:
+         * Rectangle(const std::string& name, Vec2 position, Vec2 size, const std::shared_ptr<Renderer::Interface>& renderer, const std::string& hexColor);
+         * Or maybe, move to a builder system for better readability and usability.
+         */
         Rectangle(const std::string& name, const Components::Position& position, const Components::Size& size, const std::shared_ptr<Renderer::Interface>& renderer, Components::Color color);
         void Draw() override;
         void Update() override;
